fca4e83bd9983fafdb7b9009280eb0c9811cda45
ASUS <faris13abiyu@gmail.com>
2025-11-07 21:42:09 +0700

    edit string jenis_kelamin

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -32,7 +32,7 @@ fun TampilData(
 ){
     val items = listOf(
         Pair(first = stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
-        Pair(first = stringResource(id = R.string.jeis_kelamin),"Lainnya"),
+        Pair(first = stringResource(id = R.string.jeis_kelamin),"Contoh Jenis kelamin"),
         Pair(first = stringResource(id = R.string.alamat),"Contoh Nama")
     )
     Scaffold(
