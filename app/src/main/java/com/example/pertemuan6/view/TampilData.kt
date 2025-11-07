32f959584f49c26edcd435337cef01183e888582
ASUS <faris13abiyu@gmail.com>
2025-11-07 16:21:54 +0700

    tambah scaffold di TampilData.kt

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -35,5 +35,32 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.jeis_kelamin),"Lainnya"),
         Pair(first = stringResource(id = R.string.alamat),"Contoh Nama")
     )
-
+    Scaffold(
+        modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+            )
+        }){ isiRuang->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween){
+            Column(modifier= Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))){
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick){
+                    Text(text = stringResource(id= R.string.back))
+                }
+            }
+        }
+    }
 }
